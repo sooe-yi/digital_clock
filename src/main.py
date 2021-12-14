7d26bc1134b6eeb20f40567d42d2630f36676210
大恵悟志 <oesatoshi@soe-2.local>
2021-12-14 23:47:37 +0900

    first commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from time_controller import show_time
+
+
+def main():
+    show_time()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/time_controller.py
@@ -0,0 +1,10 @@
+def show_time(self):
+    pass
+
+
+def format_time(self):
+    pass
+
+
+def get_time(self):
+    pass
